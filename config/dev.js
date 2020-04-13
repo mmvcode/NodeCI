b4645da94fa4bbfbbf5705b16e1d95a04c5f367d
Mihai VLASCEANU <me@mmvco.de>
2020-04-13 17:37:09 +0300

    Initial commit

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "539747113759-l2vqck3f6okqpvc76qvep50bna0mfogm.apps.googleusercontent.com",
+  googleClientSecret: "0lNDh-HOnylvaD7526L8wwWi",
+  mongoURI:
+    "mongodb://mmvcode:wuf4dnyJYJ3mrLAO@blogster-shard-00-00-h11bj.mongodb.net:27017,blogster-shard-00-01-h11bj.mongodb.net:27017,blogster-shard-00-02-h11bj.mongodb.net:27017/test?ssl=true&replicaSet=BlogSter-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
