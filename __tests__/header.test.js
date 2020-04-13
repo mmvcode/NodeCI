4fb95cb8c33f68e7d871409c8757d7f0377a4ad0
Mihai VLASCEANU <me@mmvco.de>
2020-04-13 17:37:09 +0300

    Initial commit

---
+++ __tests__/header.test.js
@@ -0,0 +1,30 @@
+const Page = require("./helpers/page");
+let page;
+const baseUrl = "http://localhost:3000";
+describe("header tests", () => {
+  beforeEach(async () => {
+    page = await Page.build();
+    await page.goto(baseUrl);
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  test("Header has the correct text", async () => {
+    const text = await page.getContentsOf("a.brand-logo");
+    expect(text).toEqual("Blogster");
+  });
+
+  test("Clicking login starts oauth2 flow", async () => {
+    await page.click("a[href='/auth/google']");
+    const url = page.url();
+    expect(url).toMatch(/accounts.google.com/);
+  });
+
+  test("When signed in, shows logout button", async () => {
+    await page.login();
+    const text = await page.getContentsOf("a[href='/auth/logout");
+    expect(text).toEqual("Logout");
+  });
+});
